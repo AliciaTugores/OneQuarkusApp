f8414b94622ba440fc6e3f52e07a07e56a7dfb2f
AliciaTugores <atugoresb@cifpfbmoll.eu>
2021-06-21 18:13:54 +0200

    feat: implementado método cargaItem y por consecuente pasados casos test 7 y 8

--- examen/src/main/java/pingpong/examen/ServiceOlli.java
+++ examen/src/main/java/pingpong/examen/ServiceOlli.java
@@ -14,12 +14,17 @@ import pingpong.examen.Usuaria;
 @ApplicationScoped
 public class ServiceOlli {
 
-    //caso test 5
+    //caso test 5 y 6
     public Usuaria cargaUsuaria(String name){
         Optional<Usuaria> user =  Usuaria.find("user_nom", name).firstResultOptional();
-        return user.isPresent() ? user.get(): new Usuaria();
+        return user.isPresent() ? user.get() : new Usuaria("", 0);
     }
     
+    //caso test 7 y 8
+    public Item cargaItem(String name){
+        Optional<Item> item = Item.find("item_nom", name).firstResultOptional();
+        return item.isPresent() ? item.get() : new Item("", 0, "");
+    }
 
 
 
