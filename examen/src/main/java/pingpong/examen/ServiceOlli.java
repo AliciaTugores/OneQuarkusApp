36279e5a0f4a79e8bb2fa521c74e091a1a52b365
AliciaTugores <atugoresb@cifpfbmoll.eu>
2021-06-21 18:08:24 +0200

    feat: implementado método cargarUsuaria y pasado caso test 5

--- examen/src/main/java/pingpong/examen/ServiceOlli.java
+++ examen/src/main/java/pingpong/examen/ServiceOlli.java
@@ -13,5 +13,14 @@ import pingpong.examen.Usuaria;
 
 @ApplicationScoped
 public class ServiceOlli {
+
+    //caso test 5
+    public Usuaria cargaUsuaria(String name){
+        Optional<Usuaria> user =  Usuaria.find("user_nom", name).firstResultOptional();
+        return user.isPresent() ? user.get(): new Usuaria();
+    }
     
+
+
+
 }
