1ded4221998f0579d5dea3575cb2f14508fabe62
AliciaTugores <atugoresb@cifpfbmoll.eu>
2021-06-29 17:33:59 +0200

    feat: pasado segundo caso test, implementando un GET wellcome

--- examen/src/main/java/pingpong/examen/ResourcesOlli.java
+++ examen/src/main/java/pingpong/examen/ResourcesOlli.java
@@ -1,10 +1,16 @@
 package pingpong.examen;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/")
 public class ResourcesOlli {
@@ -13,8 +19,10 @@ public class ResourcesOlli {
     ServiceOlli service;
 
     @GET
+    @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Path("/wellcome")
+    public String wellcome() {
+        return "Wellcome Ollivanders!";
     }
 }
